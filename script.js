e03892cf651c301ca16128d91ad500816d72369f
olayiwolaosho <olayiwolaosho.olinj@gmail.com>
2024-10-31 17:55:57 +0100

    changed starts to statusVal

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 // Get query parameters from URL
 const urlParams = new URLSearchParams(window.location.search);
-const status = urlParams.get('status');
+const statusVal = urlParams.get('status');
 
 // Select elements
 const statusContainer = document.querySelector('.status-container');
 const statusMessage = document.getElementById('status-message');
+  
+  // Check status and set the message and styling
+  if (statusVal === 'success' || statusVal === 'successful') {
+    statusMessage.textContent = 'Transaction Successful';
+    statusContainer.classList.add('success');
+  } else if (statusVal === 'fail'|| statusVal === 'failed') {
+    statusMessage.textContent = 'Transaction Failed';
+    statusContainer.classList.add('fail');
+  } else {
+    statusMessage.textContent = 'Invalid Transaction Status';
+  }
+  
 
-// Check status and set the message and styling
-if (status === 'success') {
-  statusMessage.textContent = 'Transaction Successful';
-  statusContainer.classList.add('success');
-} else if (status === 'fail') {
-  statusMessage.textContent = 'Transaction Failed';
-  statusContainer.classList.add('fail');
-} else {
-  statusMessage.textContent = 'Invalid Transaction Status';
-}
+
